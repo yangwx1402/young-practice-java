a790b1a6ce70015ae0a7910e1f38abefe602622f
杨勇 <yangyong@192.168.3.20>
2021-07-24 23:05:39 +0800

    init commit

---
+++ spring-practice/src/main/java/com/young/practice/App.java
@@ -0,0 +1,15 @@
+package com.young.practice;
+
+import org.apache.commons.collections.CollectionUtils;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
